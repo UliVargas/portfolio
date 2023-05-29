e50cd44a985a8446b2fef8edd4c82982d4c00ab3
Ulises Vargas <uli.vargas02@gmail.com>
2023-05-29 05:46:16 -0600

    Agrego menu de sandwitch

--- app/components/Shared/Navbar.tsx
+++ app/components/Shared/Navbar.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { GrMenu, GrClose } from 'react-icons/gr'
+import { useState } from 'react'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import classNames from 'classnames'
@@ -22,36 +24,93 @@ const routes: Route[] = [
 
 export const Navbar = () => {
   const pathname = usePathname()
+  const [isOpen, setIsOpen] = useState(false)
+
+  const openAndCloseMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <nav className="hidden md:contents">
-      {
-        <ul className="text-center md:text-left md:flex md:gap-x-6">
-          {routes.map(({ url, label }) => {
-            return (
-              <li
-                className={classNames(
-                  'text-md font-normal py-1 md:py-0 text-[#010B40] hover:text-[#127357]',
-                  {
-                    'text-[#127357]': url === pathname
-                  }
-                )}
-                key={url}
-              >
-                <Link
-                  href={url}
-                  className={
-                    pathname === url
-                      ? 'text-[#127357] underline underline-offset-4'
-                      : ''
-                  }
+    <>
+      <nav className="hidden md:flex">
+        {
+          <ul className="text-center md:text-left md:flex md:gap-x-6">
+            {routes.map(({ url, label }) => {
+              return (
+                <li
+                  className={classNames(
+                    'text-md font-normal py-1 md:py-0 text-[#010B40] hover:text-[#127357]',
+                    {
+                      'text-[#127357]': url === pathname
+                    }
+                  )}
+                  key={url}
                 >
-                  {label}
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      }
-    </nav>
+                  <Link
+                    href={url}
+                    className={
+                      pathname.startsWith(url)
+                        ? 'text-[#127357] underline underline-offset-4'
+                        : ''
+                    }
+                  >
+                    {label}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        }
+      </nav>
+
+      {/* hamburger */}
+      {isOpen ? (
+        // close button
+        <GrClose
+          onClick={openAndCloseMenu}
+          className="fixed right-[40px] text-3xl z-50 md:hidden"
+        />
+      ) : (
+        <GrMenu onClick={openAndCloseMenu} className="text-3xl md:hidden" />
+      )}
+
+      {/* mobile nav */}
+
+      <nav
+        className={`h-[100vh] fixed top-[0px] flex flex-col justify-around items-center w-full md:hidden bg-white z-40 duration-[600ms] ${
+          isOpen ? 'right-[0px]' : 'right-[-100vw]'
+        } `}
+      >
+        {
+          <ul className="text-center md:text-left md:flex md:gap-x-6">
+            {routes.map(({ url, label }) => {
+              return (
+                <li
+                  className={classNames(
+                    'text-xl font-semibold py-4 md:py-0 text-[#010B40] hover:text-[#127357]',
+                    {
+                      'text-[#127357]': url === pathname
+                    }
+                  )}
+                  key={url}
+                >
+                  <Link
+                    href={url}
+                    className={
+                      pathname === url
+                        ? 'text-[#127357] underline underline-offset-4'
+                        : ''
+                    }
+                    onClick={openAndCloseMenu}
+                  >
+                    {label}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        }
+      </nav>
+    </>
   )
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,26 +1,7 @@
-import { Repo } from '@/models/repo'
-
-const getRepos = async () => {
-  return fetch('https://api.github.com/user/repos', {
-    headers: {
-      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`
-    }
-  }).then(resp => resp.json())
-}
-
 const ProjectsPage = async () => {
-  const data: Repo[] = await getRepos()
-
-  console.log(data)
-
   return (
     <>
       <h1>Página de proyectos</h1>
-      <ul>
-        {data.map(repo => (
-          <li key={repo.id}>{repo.name}</li>
-        ))}
-      </ul>
     </>
   )
 }
