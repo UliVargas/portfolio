02e9030348b0affc2f20612429ce04c9952f9fe4
Ulises Vargas <uli.vargas02@gmail.com>
2023-05-29 06:11:35 -0600

    Divido codigo de navbar para que sea más legible

---
+++ app/components/Shared/DesktopNavbar.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+import classNames from 'classnames'
+
+import { Route } from './Navbar'
+
+export const DesktopNavbar = ({
+  routes,
+  pathname
+}: {
+  routes: Route[]
+  pathname: string
+}) => {
+  return (
+    <nav className="hidden md:flex">
+      {
+        <ul className="text-center md:text-left md:flex md:gap-x-6">
+          {routes.map(({ url, label }) => {
+            return (
+              <li
+                className={classNames(
+                  'text-md font-normal py-1 md:py-0 text-[#010B40] hover:text-[#127357]',
+                  {
+                    'text-[#127357]': url === pathname
+                  }
+                )}
+                key={url}
+              >
+                <Link
+                  href={url}
+                  className={
+                    pathname === url
+                      ? 'text-[#127357] underline underline-offset-4'
+                      : ''
+                  }
+                >
+                  {label}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      }
+    </nav>
+  )
+}
---
+++ app/components/Shared/MovilNavbar.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import { GrMenu, GrClose } from 'react-icons/gr'
+import { FC } from 'react'
+import Link from 'next/link'
+import classNames from 'classnames'
+
+import { Route } from './Navbar'
+
+interface Props {
+  routes: Route[]
+  pathname: string
+  isOpen: boolean
+  openAndClose: () => void
+}
+
+export const MovilNavbar: FC<Props> = ({
+  routes,
+  pathname,
+  openAndClose,
+  isOpen
+}) => {
+  return (
+    <>
+      {/* hamburger */}
+      {isOpen ? (
+        // close button
+        <GrClose
+          onClick={openAndClose}
+          className="fixed right-[40px] text-3xl z-50 md:hidden"
+        />
+      ) : (
+        <GrMenu onClick={openAndClose} className="text-3xl md:hidden" />
+      )}
+
+      {/* mobile nav */}
+
+      <nav
+        className={`h-[100vh] fixed top-[0px] flex flex-col justify-around items-center w-full md:hidden bg-white z-40 duration-[600ms] ${
+          isOpen ? 'right-[0px]' : 'right-[-100vw]'
+        } `}
+      >
+        {
+          <ul className="text-center md:text-left md:flex md:gap-x-6">
+            {routes.map(({ url, label }) => {
+              return (
+                <li
+                  className={classNames(
+                    'text-xl font-semibold py-4 md:py-0 text-[#010B40] hover:text-[#127357]',
+                    {
+                      'text-[#127357]': url === pathname
+                    }
+                  )}
+                  key={url}
+                >
+                  <Link
+                    href={url}
+                    className={
+                      pathname === url
+                        ? 'text-[#127357] underline underline-offset-4'
+                        : ''
+                    }
+                    onClick={openAndClose}
+                  >
+                    {label}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        }
+      </nav>
+    </>
+  )
+}
