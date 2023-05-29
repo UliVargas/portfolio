c025d0c53375834a6f1549a6e1f43b806ec3fdc6
Ulises Vargas <uli.vargas02@gmail.com>
2023-05-29 01:05:08 -0600

    Remuevo axios

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,15 +3,15 @@ import axios from 'axios'
 import { Repo } from '@/models/repo'
 
 const getRepos = async () => {
-  return axios.get<Repo[]>('https://api.github.com/user/repos', {
+  return fetch('https://api.github.com/user/repos', {
     headers: {
       Authorization: `Bearer ${process.env.GITHUB_TOKEN}`
     }
-  })
+  }).then(resp => resp.json())
 }
 
 const ProjectsPage = async () => {
-  const { data } = await getRepos()
+  const data: Repo[] = await getRepos()
   return (
     <>
       <h1>Página de proyectos</h1>
